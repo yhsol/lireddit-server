1156ce1cb293b098a39b1f5060166e8815e74b91
yhsol <yhsol1592@gowid.com>
2020-09-02 20:57:47 +0900

    feat: settings

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import mikroOrmConfig from "./mikro-orm.config";
+import express from "express";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroOrmConfig);
+  await orm.getMigrator().up();
+
+  const app = express();
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
+  //   const post = orm.em.create(Post, { title: "first post" });
+  //   await orm.em.persistAndFlush(post);
+
+  //   const posts = await orm.em.find(Post, {});
+  //   console.log(posts);
+};
+
+main().catch((err) => {
+  console.error(err);
+});
