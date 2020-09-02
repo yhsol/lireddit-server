425c6ccb4ed4baa8edd139c448e814779d20f291
yhsol <yhsol1592@gowid.com>
2020-09-02 21:09:38 +0900

    feat: settings

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,21 @@
+import { Post } from "./entities/Post";
+import { __prod__ } from "./constants";
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+require("dotenv").config();
+
+export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
+  entities: [Post],
+  password: process.env.POSTGRESQL_PASSWORD,
+  dbName: "lireddit",
+  type: "postgresql",
+  debug: !__prod__,
+} as Parameters<typeof MikroORM.init>[0];
