91b1bebef67851e34ccd9e5fddced4e46b9b0676
yhsol <yhsol1592@gowid.com>
2020-09-03 11:49:11 +0900

    feat: crud with graphql

--- dist/resolvers/post.js
+++ dist/resolvers/post.js
@@ -11,13 +11,56 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var __param = (this && this.__param) || function (paramIndex, decorator) {
     return function (target, key) { decorator(target, key, paramIndex); }
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PostResolver = void 0;
 const type_graphql_1 = require("type-graphql");
-const Post_1 = require("src/entities/Post");
+const Post_1 = require("../entities/Post");
 let PostResolver = class PostResolver {
     posts(ctx) {
         return ctx.em.find(Post_1.Post, {});
+    }
+    post(id, ctx) {
+        return ctx.em.findOne(Post_1.Post, { id });
+    }
+    createPost(title, ctx) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const post = ctx.em.create(Post_1.Post, { title });
+            yield ctx.em.persistAndFlush(post);
+            return post;
+        });
+    }
+    udpatePost(id, title, ctx) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const post = yield ctx.em.findOne(Post_1.Post, { id });
+            if (!post) {
+                return null;
+            }
+            if (typeof title !== "undefined") {
+                post.title = title;
+                yield ctx.em.persistAndFlush(post);
+            }
+            return post;
+        });
+    }
+    deeltePost(id, ctx) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                yield ctx.em.nativeDelete(Post_1.Post, { id });
+            }
+            catch (error) {
+                console.error(error);
+            }
+            return true;
+        });
     }
 };
 __decorate([
@@ -27,6 +70,38 @@ __decorate([
     __metadata("design:paramtypes", [Object]),
     __metadata("design:returntype", Promise)
 ], PostResolver.prototype, "posts", null);
+__decorate([
+    type_graphql_1.Query(() => Post_1.Post, { nullable: true }),
+    __param(0, type_graphql_1.Arg("id")), __param(1, type_graphql_1.Ctx()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Number, Object]),
+    __metadata("design:returntype", Promise)
+], PostResolver.prototype, "post", null);
+__decorate([
+    type_graphql_1.Mutation(() => Post_1.Post),
+    __param(0, type_graphql_1.Arg("title")),
+    __param(1, type_graphql_1.Ctx()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String, Object]),
+    __metadata("design:returntype", Promise)
+], PostResolver.prototype, "createPost", null);
+__decorate([
+    type_graphql_1.Mutation(() => Post_1.Post, { nullable: true }),
+    __param(0, type_graphql_1.Arg("id")),
+    __param(1, type_graphql_1.Arg("title", () => String, { nullable: true })),
+    __param(2, type_graphql_1.Ctx()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Number, String, Object]),
+    __metadata("design:returntype", Promise)
+], PostResolver.prototype, "udpatePost", null);
+__decorate([
+    type_graphql_1.Mutation(() => Boolean),
+    __param(0, type_graphql_1.Arg("id")),
+    __param(1, type_graphql_1.Ctx()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Number, Object]),
+    __metadata("design:returntype", Promise)
+], PostResolver.prototype, "deeltePost", null);
 PostResolver = __decorate([
     type_graphql_1.Resolver()
 ], PostResolver);
--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,11 +1,56 @@
-import { Resolver, Query, Ctx } from "type-graphql";
+import { Resolver, Query, Ctx, Arg, Mutation } from "type-graphql";
+import { Post } from "../entities/Post";
 import { MyContext } from "src/types";
-import { Post } from "src/entities/Post";
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() ctx: MyContext): Promise<Post[]> {
     return ctx.em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(@Arg("id") id: number, @Ctx() ctx: MyContext): Promise<Post | null> {
+    return ctx.em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title") title: string,
+    @Ctx() ctx: MyContext
+  ): Promise<Post> {
+    const post = ctx.em.create(Post, { title });
+    await ctx.em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async udpatePost(
+    @Arg("id") id: number,
+    @Arg("title", () => String, { nullable: true }) title: string,
+    @Ctx() ctx: MyContext
+  ): Promise<Post | null> {
+    const post = await ctx.em.findOne(Post, { id });
+    if (!post) {
+      return null;
+    }
+    if (typeof title !== "undefined") {
+      post.title = title;
+      await ctx.em.persistAndFlush(post);
+    }
+    return post;
+  }
+
+  @Mutation(() => Boolean)
+  async deeltePost(
+    @Arg("id") id: number,
+    @Ctx() ctx: MyContext
+  ): Promise<boolean> {
+    try {
+      await ctx.em.nativeDelete(Post, { id });
+    } catch (error) {
+      console.error(error);
+    }
+    return true;
   }
 }
